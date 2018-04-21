9f00b794e326be4f31f7e3b03986947c1f99cba1
Handrew <andrew.a.han@gmail.com>
2018-04-21 10:57:43 -0400

    changed the response for the server to split out something good about you and bad about you

--- twoserver.js
+++ twoserver.js
@@ -3,12 +3,16 @@ var http = require('http');
 var PORTgood = 7000;
 var PORTbad = 7500;
 
-function handleRequest(req, response) {
+function handleRequest1(req, response) {
     response.end("You look nice!");
 }
 
-var server1 = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
+function handleRequest2(req, response) {
+    response.end("You look just aiite~!");
+}
+
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
 
 server1.listen(PORTgood, function() {
     console.log(PORTgood);
